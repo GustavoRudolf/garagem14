96331b3bba7330c33c624d477f95d8eafff8aca6
Gustavo <gustavorudolfifc@gmail.com>
2023-08-25 13:25:14 -0300

    terminar16

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'usuario',
+    'uploader',
     'garagem',
     'rest_framework',
 ]
@@ -127,3 +131,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTH_USER_MODEL = "usuario.Usuario"
+
+MEDIA_URL = "http://localhost:8000/media/"
+MEDIA_ENDPOINT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+FILE_UPLOAD_PERMISSIONS = 0o640
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
-
 from garagem.views import AcessorioViewSet,CategoriaViewSet,CorViewSet,MarcaViewSet,VeiculoViewSet
+from usuario.router import router as usuario_router
 
 router = DefaultRouter()
 
@@ -12,10 +11,18 @@ router.register(r"categorias", CategoriaViewSet)
 router.register(r"cor", CorViewSet)
 router.register(r"marca", MarcaViewSet)
 router.register(r"veiculo", VeiculoViewSet)
-# aa
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
+
+path("api/media/", include(uploader_router.urls)),
+
+urlpatterns = [
+    path("api/", include(usuario_router.urls)),
 ]
